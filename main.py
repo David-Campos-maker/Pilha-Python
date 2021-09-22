0961a191008a5f6c60e656f77cea897c1df11847
David-Campos-maker <70824817+David-Campos-maker@users.noreply.github.com>
2021-09-22 20:53:44 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from Pilha import Pilha
+
+minhaPilha = Pilha()
+
+minhaPilha.push(minhaPilha , 1)
+minhaPilha.push(minhaPilha , 2)
+minhaPilha.push(minhaPilha , 3)
+minhaPilha.push(minhaPilha , 4)
+
+minhaPilha.print_pilha(minhaPilha)
+
+minhaPilha.pop(minhaPilha)
+
+minhaPilha.print_pilha(minhaPilha)
+
+print(minhaPilha.pilha_lenght(minhaPilha))
